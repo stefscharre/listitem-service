39a5b1c75824589130ccddafe66f708dcd46c544
Stef Scharre <57891990+stefscharre@users.noreply.github.com>
2022-01-06 10:13:37 +0100

    Add files via upload

---
+++ listitemservice/src/main/java/fact/it/listitemservice/ListitemserviceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.listitemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListitemserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListitemserviceApplication.class, args);
+    }
+
+}
